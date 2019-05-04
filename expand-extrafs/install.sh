59eb785e6edcc9d62042a32565ea609790e5f264
ushiboy <ushiboy.dev@gmail.com>
2019-05-04 10:36:34 +0900

    add readonlyfs check

--- expand-extrafs/install.sh
+++ expand-extrafs/install.sh
@@ -1,7 +1,18 @@
 #!/bin/sh
 
+is_enable_readonlyfs()
+{
+    grep "\<readonlyfs\>" /proc/cmdline > /dev/null 2>&1 && return 0
+    return 1
+}
+
 if [ "`whoami`" != "root" ]; then
     echo "Require root privilege"
+    exit 1
+fi
+
+if is_enable_readonlyfs; then
+    echo "Require disable readolyfs"
     exit 1
 fi
 
